4891129a3ba71710f8ffc88da0fcb2aad6254d11
everyoneisgross <fraser.austin@gmail.com>
2023-08-07 02:05:25 +1200

    embedsumBOT.py
    added "memorise" for just inputting text directly
    added chunking to the summary method
    broke and moved and changed some things

--- embedsumBOT.py
+++ embedsumBOT.py
@@ -8,6 +8,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 from rake_nltk import Rake
 import os
 
+
 # Get the current working directory
 cwd = os.getcwd()
 
@@ -17,22 +18,31 @@ embedder = Embed4All()
 sentiment_analyzer = SentimentIntensityAnalyzer()
 keyword_extractor = Rake()
 
-# Load the database from a pickle file
-try:
-    with open('summary_embeddings.pkl', 'rb') as f:
-        database = pickle.load(f)
-except FileNotFoundError:
-    database = {}
-from scipy.spatial.distance import cosine
+def chunk_text(text, max_tokens=250):
+    """Split the text into chunks of approximately max_tokens tokens."""
+    words = text.split()
+    chunks = []
+    current_chunk = []
+    current_length = 0
+    for word in words:
+        if current_length + len(word) <= max_tokens:
+            current_chunk.append(word)
+            current_length += len(word)
+        else:
+            chunks.append(' '.join(current_chunk))
+            current_chunk = [word]
+            current_length = len(word)
+    chunks.append(' '.join(current_chunk))
+    return chunks
 
-#def euclidean_distance(c1, c2):
-#    # Convert the Counters to vectors
-#    terms = set(c1).union(c2)
-#    vec1 = [c1.get(k, 0) for k in terms]
-#    vec2 = [c2.get(k, 0) for k in terms]
-#
-#    # Compute the Euclidean distance
-#    return math.sqrt(sum((x - y) ** 2 for x, y in zip(vec1, vec2)))
+def filter_repeated_words_keywords(keywords):
+    """Filter out keyword phrases that contain repeated words."""
+    filtered_keywords = []
+    for keyword in keywords:
+        words = keyword.split()
+        if len(words) == len(set(words)):
+            filtered_keywords.append(keyword)
+    return filtered_keywords
 
 def cosine_similarity(c1, c2):
     set1 = set(c1)
@@ -44,51 +54,67 @@ def cosine_similarity(c1, c2):
     else:
         return float(len(intersection)) / len(union)
 
+# Load the database from a pickle file
+try:
+    with open('summary_embeddings.pkl', 'rb') as f:
+        database = pickle.load(f)
+except FileNotFoundError:
+    database = {}
+
 while True:
-    # Get the user's input
     user_input = input("Enter a command: ")
 
-    # If the user wants to summarize a document
-    # Import the os library
-
-
-    # If the user wants to summarize a local .txt file
-    if user_input.startswith("summarize"):
-        # Get the file name from the user input
-        file_name = user_input[len("summarize"):].strip()
-
-        # Check if the file exists and has a .txt extension
+    # If the user wants to summarise a local .txt file
+    if user_input.startswith("summarise"):
+        file_name = user_input[len("summarise"):].strip()
         if os.path.exists(file_name) and file_name.endswith(".txt"):
-            # Open the file and read its content
             with open(file_name, "r") as f:
                 document = f.read()
-
-            # Generate the summary
-            summary = gpt_model.generate(f"Summarize: {document}", max_tokens=500)
-
-            # Embed the summary
-            embedding = embedder.embed(summary)
-
-            # Analyze the sentiment of the summary
-            sentiment = sentiment_analyzer.polarity_scores(summary)
-
-            # Extract keywords from the summary
-            keyword_extractor.extract_keywords_from_text(summary)
-            keywords = keyword_extractor.get_ranked_phrases()
-
-            # Save the summary, its embedding, its sentiment, and its keywords to the database
-            database[summary] = {
-                'embedding': embedding,
-                'sentiment': sentiment,
-                'keywords': Counter(keywords)  # Save keywords as a Counter for euclidean distance
-            }
-
-            print(f"Summary: {summary}")
-            print(f"Sentiment: {sentiment}")
-            print(f"Keywords: {keywords}")
-
+                
+                # Extract keywords from the original chunk
+                keyword_extractor.extract_keywords_from_text(document)
+                data = {}
+                original_chunk_keywords = Counter(keyword_extractor.get_ranked_phrases())
+                # Split the document into chunks
+                chunks = chunk_text(document)
+            
+                for chunk in chunks:
+                    summary = gpt_model.generate(f"Summarise: {chunk}", max_tokens=150, temp=0.3)
+                    embedding = embedder.embed(summary)
+                    sentiment = sentiment_analyzer.polarity_scores(summary)
+                    # Combine keywords from the original chunk and the summary
+                    combined_keywords = original_chunk_keywords + Counter(keyword_extractor.get_ranked_phrases())
+                    data['keywords'] = combined_keywords.most_common()  # Store combined keywords in the database
+                    keywords = keyword_extractor.get_ranked_phrases()
+                    keywords = filter_repeated_words_keywords(keywords)
+                    
+                    database[summary] = {
+                        'embedding': embedding,
+                        'sentiment': sentiment,
+                        'keywords': Counter(keywords)
+                    }
+                    print(f"Summary: {summary}")
+                    print(f"Sentiment: {sentiment}")
+                    #print(f"Keywords: {keywords}")
         else:
             print("Invalid file name. Please enter a valid .txt file name.")
+
+    # If the user wants to memorise and summarise typed text
+    elif user_input.startswith("memorise"):
+        typed_text = user_input[len("memorise"):].strip()
+        summary = gpt_model.generate(f"Summarise: {typed_text}", max_tokens=500)
+        embedding = embedder.embed(summary)
+        sentiment = sentiment_analyzer.polarity_scores(summary)
+        keyword_extractor.extract_keywords_from_text(summary)
+        keywords = keyword_extractor.get_ranked_phrases()
+        database[summary] = {
+            'embedding': embedding,
+            'sentiment': sentiment,
+            'keywords': Counter(keywords)
+        }
+        print(f"Summary: {summary}")
+        print(f"Sentiment: {sentiment}")
+        #print(f"Keywords: {keywords}")
 
     # If the user wants to search for a summary
     elif user_input.startswith("search"):
@@ -125,7 +151,7 @@ while True:
         if best_summary is not None:
             print(f"Best match: {best_summary}")
             print(f"Sentiment: {database[best_summary]['sentiment']}")
-            print(f"Keywords: {database[best_summary]['keywords']}")
+            #print(f"Keywords: {database[best_summary]['keywords']}")
         else:
             print("No matches found.")
 
@@ -135,7 +161,7 @@ while True:
 
     # If the user's input is not recognized
     else:
-        print("Command not recognized. Please enter 'summarize', 'search', or 'quit'.")
+        print("Command not recognized. Please enter 'summarise', 'memorise', 'search', or 'quit'.")
 
 # Save the database to a pickle file
 with open('summary_embeddings.pkl', 'wb') as f:
